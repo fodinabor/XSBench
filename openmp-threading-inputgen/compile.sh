6fb09c589fef644a6681ad013eeced064e1991d3
Joachim Meyer <joachim@joameyer.de>
2024-07-11 15:48:44 -0700

    Don't hardcode path

--- openmp-threading-inputgen/compile.sh
+++ openmp-threading-inputgen/compile.sh
@@ -2,8 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 OUTPUT_FILE="$SCRIPT_DIR/results.$(date -Iseconds).out"
+LLVM_INSTALL_DIR=/usr/WS1/$USER/opt/input-gen-main-quartz
+LLVM_SRC_DIR=/usr/WS1/$USER/inputgen-llvm-main
 
-. ../../inputgen-llvm-main/scripts/enable.sh /usr/WS1/$USER/opt/input-gen-main-quartz
+
+. enable.sh $LLVM_INSTALL_DIR
 
 #make clean
 make -j4 default
@@ -15,9 +18,9 @@ export VERBOSE=1
 export TIMING=1
 
 mkdir -p /tmp/$USER/XS0/
-input-gen mod.bc --output-dir=/tmp/$USER/XS0/ --input-gen-runtime=/usr/WS1/$USER/inputgen-llvm-main/input-gen-runtimes/rt-input-gen.cpp --input-run-runtime=/usr/WS1/$USER/inputgen-llvm-main/input-gen-runtimes/rt-run.cpp 
-clang++ "-ldl" "-rdynamic" /tmp/$USER/XS0//input-gen.module.generate.bc /usr/WS1/$USER/inputgen-llvm-main/input-gen-runtimes/rt-input-gen.cpp "-o" "/tmp/$USER/XS0//input-gen.module.generate.a.out" "-O1" "-DNDEBUG"  -march=native
-clang++ "-ldl" "-rdynamic" /tmp/$USER/XS0//input-gen.module.run.bc /usr/WS1/$USER/inputgen-llvm-main/input-gen-runtimes/rt-run.cpp "-o" "/tmp/$USER/XS0//input-gen.module.run.a.out" "-O1" "-DNDEBUG"  -march=native
+input-gen mod.bc --output-dir=/tmp/$USER/XS0/ --input-gen-runtime=$LLVM_SRC_DIR/input-gen-runtimes/rt-input-gen.cpp --input-run-runtime=$LLVM_SRC_DIR/input-gen-runtimes/rt-run.cpp 
+clang++ "-ldl" "-rdynamic" /tmp/$USER/XS0//input-gen.module.generate.bc $LLVM_SRC_DIR/input-gen-runtimes/rt-input-gen.cpp "-o" "/tmp/$USER/XS0//input-gen.module.generate.a.out" "-O1" "-DNDEBUG"  -march=native
+clang++ "-ldl" "-rdynamic" /tmp/$USER/XS0//input-gen.module.run.bc $LLVM_SRC_DIR/input-gen-runtimes/rt-run.cpp "-o" "/tmp/$USER/XS0//input-gen.module.run.a.out" "-O1" "-DNDEBUG"  -march=native
 
 
 for i in $(seq 0 30); do
@@ -33,9 +36,9 @@ done
 echo "---- O3 -----" >> $OUTPUT_FILE
 
 mkdir -p /tmp/$USER/XS1/
-input-gen modopt.bc --output-dir=/tmp/$USER/XS1/ --input-gen-runtime=/usr/WS1/$USER/inputgen-llvm-main/input-gen-runtimes/rt-input-gen.cpp --input-run-runtime=/usr/WS1/$USER/inputgen-llvm-main/input-gen-runtimes/rt-run.cpp 
-clang++ "-ldl" "-rdynamic" /tmp/$USER/XS1//input-gen.module.generate.bc /usr/WS1/$USER/inputgen-llvm-main/input-gen-runtimes/rt-input-gen.cpp "-o" "/tmp/$USER/XS1//input-gen.module.generate.a.out" "-O3" "-DNDEBUG"  -march=native
-clang++ "-ldl" "-rdynamic" /tmp/$USER/XS1//input-gen.module.run.bc /usr/WS1/$USER/inputgen-llvm-main/input-gen-runtimes/rt-run.cpp "-o" "/tmp/$USER/XS1//input-gen.module.run.a.out" "-O3" "-DNDEBUG"  -march=native
+input-gen modopt.bc --output-dir=/tmp/$USER/XS1/ --input-gen-runtime=$LLVM_SRC_DIR/input-gen-runtimes/rt-input-gen.cpp --input-run-runtime=$LLVM_SRC_DIR/input-gen-runtimes/rt-run.cpp 
+clang++ "-ldl" "-rdynamic" /tmp/$USER/XS1//input-gen.module.generate.bc $LLVM_SRC_DIR/input-gen-runtimes/rt-input-gen.cpp "-o" "/tmp/$USER/XS1//input-gen.module.generate.a.out" "-O3" "-DNDEBUG"  -march=native
+clang++ "-ldl" "-rdynamic" /tmp/$USER/XS1//input-gen.module.run.bc $LLVM_SRC_DIR/input-gen-runtimes/rt-run.cpp "-o" "/tmp/$USER/XS1//input-gen.module.run.a.out" "-O3" "-DNDEBUG"  -march=native
 
 for i in $(seq 0 30); do
 time /tmp/$USER/XS1/input-gen.module.generate.a.out /tmp/$USER/XS1/ $i $(($i + 1)) --file /tmp/$USER/XS1/available_functions 6
